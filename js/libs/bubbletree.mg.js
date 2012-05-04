09288f43b515abc4dce714105a4ea7c5e5c6d5e8
Friedrich Lindenberg <friedrich@pudo.org>
2012-05-04 22:40:42 +0200

    Use COFOG and try to fix IE issues.

--- js/libs/bubbletree.mg.js
+++ js/libs/bubbletree.mg.js
@@ -6,6 +6,19 @@
 var BubbleTree = BubbleTree || {};
 BubbleTree.Styles = BubbleTree.Styles || {};
 
+BubbleTree.Styles.Cofog1 = {
+	'01': { icon: 'government.svg', color: '#C75746' },
+	'02': { icon: 'defence.svg', color: '#0AB971' },
+	'03': { icon: 'order-safety.svg', color: '#EC2406' }, 
+	'04': { icon: 'social-systems.svg', color: '#790586' }, 
+	'05': { icon: 'environment.svg', color: '#2A3A03' },	
+	'06': { icon: 'our-streets.svg', color: '#D33673' },		
+	'07': { icon: 'health.svg', color: '#4E6D00' },											
+	'08': { icon: 'culture.svg', color: '#938626' },					
+	'09': { icon: 'education.svg' },			
+	'10': { icon: 'helping-others.svg', color: '#935B3B' }	
+}
+
 BubbleTree.Styles.Funcao = {
 	'28': { icon: 'government-uk.svg', color: '#C75746' },
 	'1': { icon: 'legislative.svg' },
